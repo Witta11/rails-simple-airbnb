06ee86e2d4e45e6102b18ad0781f9eab466eb8e2
Witta Glöckner <alina.glocke@gmail.com>
2020-11-11 21:03:34 +0100

    index and new form

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,4 +3,22 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(set_params)
+    if @flat.save
+      redirect_to @flats
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_params
+    params(:flat).require(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
