67a999df00b2b80c5c68644aef619b888a9025b3
Witta Glöckner <alina.glocke@gmail.com>
2020-11-11 17:11:03 +0100

    set up and index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "seeding started..."
+
+Flat.destroy_all
+
+4.times do
+  Flat.create!(
+    name: Faker::BossaNova.song,
+    address: Faker::Address.full_address,
+    description: Faker::TvShows::Friends.quote,
+    price_per_night: rand(20..1000),
+    number_of_guests: rand(1..8)
+  )
+end
+
+puts "seeding ended..."
